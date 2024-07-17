378047fe96ce14a3c3e017a70da5673cd6eb8835
LJH <skRtl365@gmail.com>
2024-07-17 10:36:19 +0900

    fc경로 HTTPS로 변경

--- fcKoTime.js
+++ fcKoTime.js
@@ -1,12 +1,16 @@
 export async function koreaTime() {
     try {
-        const response = await fetch('http://worldtimeapi.org/api/timezone/Asia/Seoul');
+        const response = await fetch('https://worldtimeapi.org/api/timezone/Asia/Seoul');
+        if (!response.ok) {
+            throw new Error('인터넷 연결이 원활하지 않습니다.');
+        }
         const data = await response.json();
         const koreaTime = new Date(data.datetime).toLocaleString('ko-KR', {
             timeZone: 'Asia/Seoul',
             hour: '2-digit',
             minute: '2-digit'
         });
+        console.log('Korea Time:', koreaTime); // 콘솔에 시간 로그 추가
         document.getElementById('lookTime').innerText = koreaTime;
     } catch (error) {
         console.error('시간을 불러올 수 없습니다.:', error);
