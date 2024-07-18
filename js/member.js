69546a442a7db876384cbbc74c7d7542c4e93dd7
unknown <lklll1212@gmail.com>
2024-07-18 10:49:16 +0900

    버튼이 작동하게 하고 있지만 아직 저장은 안됩니다!

--- js/member.js
+++ js/member.js
@@ -1,3 +1,23 @@
 function openclose() {
   $("#intro-bottombtn").toggle();
 }
+
+
+$("#togglebtn").click(async function () {
+  $('#cardbox').toggle();
+})
+
+$("#postingbtn").click(async function () {
+  let photoname = $('#photoname').val();
+  let phototext = $('#phototext').val();
+  let photourl = $('#photourl').val();
+
+  let doc = {
+    'photoname': photoname,
+    'phototext': phototext,
+    'photourl': photourl
+
+  };
+  alert('사진이 저장됨');
+
+})
