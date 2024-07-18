c8cc1d761c1f1387843021e8b3a5498dee6eea26
unknown <lklll1212@gmail.com>
2024-07-18 15:53:47 +0900

    이민혁 파일 충돌전

--- js/photo.js
+++ js/photo.js
@@ -1,18 +1,34 @@
+
 $("#togglebtn").click(async function () {
     $('#cardbox').toggle();
-  })
-  
-  $("#postingbtn").click(async function () {
+})
+
+function makecard() {
+
     let photoname = $('#photoname').val();
     let phototext = $('#phototext').val();
     let photourl = $('#photourl').val();
-  
-    let doc = {
-      'photoname': photoname,
-      'phototext': phototext,
-      'photourl': photourl
-  
-    };
+
+    let temp_html = `
+                <div class="col">
+                                        <div class="card">
+                                            <img src="${photourl}"
+                                                class="card-img-top" alt="...">
+                                            <div class="card-body">
+                                                <h5 class="card-title">${photoname}</h5>
+                                                <p class="card-text">${phototext}</p>
+                                            </div>
+                                        </div>
+                                    </div>`;
+
+    $('#cardbox').append(temp_html);
+   
+
     alert('사진이 저장됨');
-  
-  })
+
+
+
+
+}
+
+
