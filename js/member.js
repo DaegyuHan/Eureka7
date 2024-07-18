bd817f3731e46685963109a3be28598171d843ce
Xabier <94465466+XabierJEONG@users.noreply.github.com>
2024-07-18 13:51:26 +0900

    jinho rev3

--- js/member.js
+++ js/member.js
@@ -1,56 +1,25 @@
-/*
-function openclose() {
-  $("#intro-bottomtog").toggle();
-}
-*/
-
-/*
-$("#postingbtn").click(async function () {
-  let image = $("#image").val();
-  let comment = $("#comment").val();
-
-  let doc = {
-    'image': image,
-    'comment': comment
-  };
-  let docs = await getDocs(collection(db, "movies"));
-  docs.forEach((doc) => {
-    let row = doc.data();
-    let image = row['image'];
-    let comment = row['comment'];
-
-    <div class="intro-bottom" id='intro-bottomtog'>
-      <div class="bottom-image">
-        <img id="personal-image" src="${image}" alt="개인사진">
-      </div>
-      <div class="bottom-intro">
-        <p>${comment}개인 소개내용</p>
-      </div>
-    </div>
-    $("#intro-bottomtog").append(temp_html);
-  });
-*/
-
-
+//팀원소개페이지 script//
 document.querySelectorAll('.btn').forEach(button => {
   button.addEventListener('click', () => {
     const targetId = button.getAttribute('data-target');
     const content = document.getElementById(targetId);
 
-    // 모든 토글 콘텐츠를 닫음
     document.querySelectorAll('.toggle-content').forEach(div => {
-      div.style.display = 'none';
+      if (div.id !== targetId) {
+        div.style.display = 'none';
+      }
     });
 
-    // 클릭한 버튼의 토글 콘텐츠를 토글
-    if (content.style.display === 'none' || content.style.display === '') {
-      content.style.display = 'grid';
-    } else {
+    if (content.style.display === 'grid') {
       content.style.display = 'none';
+    } else {
+      content.style.display = 'grid';
     }
   });
 });
 
+
+//추억앨범페이지 scirpt//
 $("#togglebtn").click(async function () {
   $('#cardbox').toggle();
 })
