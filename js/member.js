552aa854f0e85b517ff9e2905fd474e7d2573dc5
Xabier <94465466+XabierJEONG@users.noreply.github.com>
2024-07-18 17:46:10 +0900

    jinho rev3

--- js/member.js
+++ js/member.js
@@ -17,27 +17,3 @@ document.querySelectorAll('.btn').forEach(button => {
     }
   });
 });
-
-<<<<<<< HEAD
-=======
-
-//추억앨범페이지 scirpt//
-$("#togglebtn").click(async function () {
-  $('#cardbox').toggle();
-})
-
-$("#postingbtn").click(async function () {
-  let photoname = $('#photoname').val();
-  let phototext = $('#phototext').val();
-  let photourl = $('#photourl').val();
-
-  let doc = {
-    'photoname': photoname,
-    'phototext': phototext,
-    'photourl': photourl
-
-  };
-  alert('사진이 저장됨');
-
-})
->>>>>>> 0ee9814be9bd09d0b2ac0777ca1264bda0ef9574
