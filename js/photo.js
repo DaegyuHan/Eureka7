47ef6a1d33fb8c62d26985dd4e808d63fe423b0e
LJH <skRtl365@gmail.com>
2024-07-19 09:38:18 +0900

    토글수정

--- js/photo.js
+++ js/photo.js
@@ -1,34 +1,25 @@
-
-$("#togglebtn").click(async function () {
-    $('#cardbox').toggle();
-})
+(document).ready(function() {
+    $("#togglebtn").click(function() {
+        $('#cardbox').toggle();
+    });
+});
 
 function makecard() {
-
     let photoname = $('#photoname').val();
     let phototext = $('#phototext').val();
     let photourl = $('#photourl').val();
 
     let temp_html = `
-                <div class="col">
-                                        <div class="card">
-                                            <img src="${photourl}"
-                                                class="card-img-top" alt="...">
-                                            <div class="card-body">
-                                                <h5 class="card-title">${photoname}</h5>
-                                                <p class="card-text">${phototext}</p>
-                                            </div>
-                                        </div>
-                                    </div>`;
-
+        <div class="col">
+            <div class="card">
+                <img src="${photourl}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${photoname}</h5>
+                    <p class="card-text">${phototext}</p>
+                </div>
+            </div>
+        </div>`;
+    
     $('#cardbox').append(temp_html);
-   
-
     alert('사진이 저장됨');
-
-
-
-
 }
-
-
